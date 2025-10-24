6c4c1f165cd16472fb263c223b9f69c279923c4f
Logan <34078184+SentientDragon5@users.noreply.github.com>
2025-10-24 16:25:07 -0700

    add click functions, and agent example

--- scripts/dog.gd
+++ scripts/dog.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+@onready var agent : NavigationAgent2D = $NavigationAgent2D
+
+@export var move_speed : float = 2000
+var goal : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +13,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+	
+	if not agent.is_navigation_finished():
+		velocity = global_position.direction_to(agent.get_next_path_position()) * move_speed
 
 func _mouse_enter() -> void:
 	#print(name, " mouse enter")
@@ -24,3 +31,9 @@ func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> vo
 			if event.button_index == MOUSE_BUTTON_LEFT:
 				print(name, " has been clicked")
 				pass
+				
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				print("mouse clicked at ", get_global_mouse_position())
