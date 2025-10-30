3a48d26e9702c1683784a1742c65653d6c47996f
Aryan Sidbatte <15204110+aryansidbatte@users.noreply.github.com>
2025-10-30 14:20:46 -0700

    Implemented Sheep runaway behavior and Added SheepSpawner

--- scripts/sheep.gd
+++ scripts/sheep.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
-@export var _speed = 50
+@export var _speed = 20
 var _direction = Vector2()
+
+var threatening_dogs = []
 
 
 func _ready() -> void:
@@ -9,6 +11,27 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	velocity = _direction * _speed
-	rotation = atan2(_direction.y, _direction.x) + deg_to_rad(90)
-	move_and_slide()
+	var flee_direction = Vector2.ZERO
+	for dog in threatening_dogs:
+		var diff = global_position - dog.global_position
+		if diff.length() > 0.1:
+			flee_direction += diff.normalized()
+	if flee_direction != Vector2.ZERO:
+		flee_direction = flee_direction.normalized()
+		velocity = flee_direction * _speed
+		rotation = atan2(flee_direction.y, flee_direction.x) + deg_to_rad(90)
+		move_and_slide()
+	else:
+		# idle or other behavior if not threatened
+		velocity = _direction * _speed
+		rotation = atan2(_direction.y, _direction.x) + deg_to_rad(90)
+		move_and_slide()
+
+func start_fleeing_from(dog):
+	if dog not in threatening_dogs:
+		threatening_dogs.append(dog)
+
+func stop_fleeing_from(dog):
+	threatening_dogs.erase(dog)
+	
+	
