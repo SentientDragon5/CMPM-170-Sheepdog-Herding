ae9bd6d683cd2a353e27ad6bfba18487a2073663
Thryloz <86812547+Thryloz@users.noreply.github.com>
2025-11-05 14:41:08 -0800

    FINALLY FIXED DOG SELECTION

--- scripts/dog.gd
+++ scripts/dog.gd
@@ -2,9 +2,6 @@ extends CharacterBody2D
 
 @onready var agent : NavigationAgent2D = $NavigationAgent2D
 
-
-@onready var clickableArea: Area2D = $Area2D
-
 @onready var wolf_sprite: Sprite2D = $WolfSprite
 @onready var selected_ui: Sprite2D = $Selected
 @onready var goal_arrow: Sprite2D = $GoalArrow
@@ -18,17 +15,17 @@ extends CharacterBody2D
 func _ready() -> void:
 	_selected = false
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	click_navigation()
 	navigate()
 
 func click_navigation() -> void:
 	if Input.is_action_just_pressed("left_click") and _selected:
-		if not wolf_sprite.is_pixel_opaque(get_local_mouse_position()):
 			_selected = false
 			selected_ui.visible = false
 			agent.target_position  = get_global_mouse_position()
 			goal_arrow.visible = true
+
 
 
 func navigate() -> void:
@@ -39,20 +36,21 @@ func navigate() -> void:
 	var new_velocity:Vector2 = (
 		global_position.direction_to(next_path_position) * move_speed
 	)
-	goal_arrow.global_position = agent.target_position
 	agent.velocity = new_velocity
-	wolf_sprite.rotation = new_velocity.angle() + deg_to_rad(90)
+	rotation = new_velocity.angle() + deg_to_rad(90)
+	goal_arrow.global_position = agent.target_position
+	goal_arrow.global_rotation = 0
 
-func _input_event(_viewport, event, _shape_idx):
-	if event.is_action_pressed("left_click"):
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	position += safe_velocity * get_physics_process_delta_time()
+	
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_released("left_click"):
 		if not _selected:
 			_selected = true
 			selected_ui.visible = true
 		else:
 			_selected = false
 			selected_ui.visible = false
-
-
-func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
-	position += safe_velocity * get_physics_process_delta_time()
-	
