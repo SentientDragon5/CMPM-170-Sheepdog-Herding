d16350ce583df8bfff00f7b91b9c06541eb3003e
Thryloz <86812547+Thryloz@users.noreply.github.com>
2025-11-05 14:20:05 -0800

    condensed into flock function

--- scripts/sheep.gd
+++ scripts/sheep.gd
@@ -1,9 +1,9 @@
 extends RigidBody2D
 
 @export var _speed = 20
-@export var _alignment_multiplier = 40
-@export var _cohesion_multiplier = 40
-@export var _separation_multiplier = 40
+@export var _alignment_multiplier = 2
+@export var _cohesion_multiplier = 2
+@export var _separation_multiplier = 2
 var _direction = Vector2()
 
 var threatening_dogs = []
@@ -16,9 +16,7 @@ func _ready() -> void:
 
 
 func _physics_process(_delta: float) -> void:
-	_direction += _alignment()
-	_direction += _cohesion()
-	_direction += _separation()
+	_direction += _flock_behavior()
 	linear_velocity = _direction.normalized() * _speed
 	rotation = atan2(_direction.y, _direction.x) + deg_to_rad(90)
 	
@@ -28,44 +26,34 @@ func _on_detection_body_entered(body: Node2D) -> void:
 	else:
 		_direction = -_direction
 
-		
 func _on_detection_body_exited(body: Node2D) -> void:
 	if(body.is_in_group("Sheep")):
 		neighboring_sheep.erase(body)
-
-func _alignment() -> Vector2: 
+		
+func _flock_behavior() -> Vector2:
 	if neighboring_sheep.size() == 0:
 		return _direction
-
-	var average_velocity = Vector2.ZERO
-	for sheep in neighboring_sheep:
-		average_velocity += sheep.linear_velocity 
-	average_velocity /= neighboring_sheep.size()
-		
-	return average_velocity 
 	
-func _cohesion() -> Vector2: 
-	if neighboring_sheep.size() == 0:
-		return _direction
-
-	var average_location = Vector2.ZERO
+	var alignment_vector = Vector2.ZERO
+	var cohesion_vector = Vector2.ZERO 
+	var separation_vector = Vector2.ZERO
+	
 	for sheep in neighboring_sheep:
-		average_location += sheep.global_position 
-	average_location /= neighboring_sheep.size()
-		
-	return global_position.direction_to(average_location) 
-
-func _separation() -> Vector2: 
-	if neighboring_sheep.size() == 0:
-		return _direction
-
-	var separation = Vector2.ZERO
-	for sheep in neighboring_sheep:
-		separation += ((global_position - sheep.global_position) / clamp(global_position.distance_to(sheep.global_position), 0.01 , INF))
-	separation /= neighboring_sheep.size()
-	return separation
+		alignment_vector += sheep.linear_velocity 
+		cohesion_vector += sheep.global_position
+		separation_vector += ((global_position - sheep.global_position) / clamp(global_position.distance_to(sheep.global_position), 0.01 , INF))
+	
+	alignment_vector /= neighboring_sheep.size() 
+	cohesion_vector = global_position.direction_to(cohesion_vector/neighboring_sheep.size())
+	separation_vector /= neighboring_sheep.size()
+	
+	alignment_vector *= _alignment_multiplier 
+	cohesion_vector *= _cohesion_multiplier 
+	separation_vector *= _separation_multiplier 
+	
+	return alignment_vector + cohesion_vector + separation_vector
 
 # backup reflection
 func _on_body_entered(body: Node) -> void:
 	if (body.name == "Rocks"):
-		_direction = -position.direction_to(body.position)
+		_direction = -_direction
