fb086e5c6ab243bbe3d05f4a269d0dcd6fae3212
Thryloz <86812547+Thryloz@users.noreply.github.com>
2025-11-05 11:32:49 -0800

    tilemap and camera

--- scripts/sheep.gd
+++ scripts/sheep.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 @export var _speed = 20
+@onready var _detection_radius = $Detection/CollisionShape2D.shape.radius
 var _direction = Vector2()
 
 var threatening_dogs = []
@@ -13,15 +14,10 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	_direction = _alignment()
+	_direction += _alignment() * _speed
 	linear_velocity = _direction.normalized() * _speed
 	rotation = atan2(_direction.y, _direction.x) + deg_to_rad(90)
 	
-
-func _on_body_entered(body: Node) -> void:
-	_direction = (position - body.position).normalized()
-
-
 func _on_detection_body_entered(body: Node2D) -> void:
 	if(body.is_in_group("Sheep")):
 		neighboring_sheep.append(body)
@@ -29,13 +25,15 @@ func _on_detection_body_entered(body: Node2D) -> void:
 func _on_detection_body_exited(body: Node2D) -> void:
 	if(body.is_in_group("Sheep")):
 		neighboring_sheep.erase(body)
-	
+
 func _alignment() -> Vector2: 
 	if neighboring_sheep.size() == 0:
 		return _direction
-	var average_direction = Vector2.ZERO
+
+	var average_velocity = Vector2.ZERO
 	for sheep in neighboring_sheep:
-		average_direction += sheep.linear_velocity 
-	average_direction /= neighboring_sheep.size()
-	return average_direction
+		average_velocity += sheep.linear_velocity 
+	average_velocity /= neighboring_sheep.size()
+		
+	return average_velocity 
 	
